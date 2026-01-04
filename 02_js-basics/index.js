6cea9cc00580b742180343c655df73a8857612ae
nihal <nihalsheikh585@gmail.com>
2026-01-04 21:46:26 +0530

    JS Basic

---
+++ 02_js-basics/index.js
@@ -0,0 +1,18 @@
+// var, let, const
+// var a = 1;
+// a = 2;
+// console.log(a);
+
+let firstName = "John";
+let age = 24;
+let isMarried = "monkey";
+
+// console.log("This person's name is " + firstName + " and age is " + age);
+
+if (isMarried == true) {
+	console.log(firstName + " is married.");
+}
+
+if (isMarried == false) {
+	console.log(firstName + " is not married.");
+}
