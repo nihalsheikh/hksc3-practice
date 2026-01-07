1da7039ade5c284d506a27ca440647e542c4d9e5
nihal <nihalsheikh585@gmail.com>
2026-01-07 11:38:11 +0530

    js-basics: Number methods

---
+++ 03_week-01-js-refresher/level-1/02-number.js
@@ -0,0 +1,7 @@
+// Parse Integer ( convert str to int -> "42" => 42 )
+console.log(parseInt("42"));
+console.log(parseInt("3.14")); // 3.14 -> 3 (float is converted to int)
+
+// Parse Float
+console.log(parseFloat("212")); // (int is converted to float)
+console.log(parseFloat("3.14"));
