fd1b04d1b67fc97cdede08f21d5a3da4b7184a99
nihal <nihalsheikh585@gmail.com>
2026-01-07 21:43:09 +0530

    Date class

---
+++ 03_week-01-js-refresher/level-2/02-date.js
@@ -0,0 +1,6 @@
+// Date class
+const currDate = new Date();
+console.log(currDate);
+console.log(currDate.getMonth() + 1); // January is 0, December is 11
+console.log(currDate.getDate());
+console.log(currDate.getFullYear());
