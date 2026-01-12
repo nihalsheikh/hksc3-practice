b8d5e370a9fb60a97fb4b2b483e1fa7ff2c93938
nihal <nihalsheikh585@gmail.com>
2026-01-12 21:26:01 +0530

    Send JSON Data of Patient Kidneys

--- 04_express-nodejs/hospital-server/index.js
+++ 04_express-nodejs/hospital-server/index.js
@@ -11,14 +11,31 @@ var users = [
 ];
 
 app.get("/", (req, res) => {
-	res.send("Hospital Waiting Area");
+	const johnKidneys = users[0].kidneys;
+	const totalKidneys = johnKidneys.length;
+
+	const numberOfHealthyKidneys = 0;
+
+	for (let i = 0; i < johnKidneys; i++) {
+		if (johnKidneys[i].healthy) {
+			numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+		}
+	}
+
+	const numberOfUnhealthyKidneys = totalKidneys - numberOfHealthyKidneys;
+
+	res.send({
+		totalKidneys,
+		numberOfHealthyKidneys,
+		numberOfUnhealthyKidneys,
+	});
 });
 
-app.post("/", (req, res) => {});
+// app.post("/", (req, res) => {});
 
-app.put("/", (req, res) => {});
+// app.put("/", (req, res) => {});
 
-app.delete("/", (req, res) => {});
+// app.delete("/", (req, res) => {});
 
 app.listen(PORT, () => {
 	console.log(`server is listening on: http://localhost:${PORT}`);
