2b2ca7bf34e4394e2313b0a22cc573b2ef532d8a
nihal <nihalsheikh585@gmail.com>
2026-01-16 19:31:29 +0530

    Middleware, Authentication, Global Catches, Zod

---
+++ 05_middleware-auth-zod/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+	res.send(`Hello: This is Port: ${PORT}`);
+});
+
+app.listen(PORT, function () {
+	console.log(`server is listening on: http://localhost:${PORT}`);
+});
