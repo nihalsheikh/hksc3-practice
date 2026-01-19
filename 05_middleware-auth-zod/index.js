c8f4e51f99dd223848130cba8b5af30c41fcd2d1
nihal <nihalsheikh585@gmail.com>
2026-01-19 22:49:18 +0530

    Headers and Query Params

--- 05_middleware-auth-zod/index.js
+++ 05_middleware-auth-zod/index.js
@@ -3,12 +3,52 @@ const app = express();
 
 const PORT = 3000;
 
+// Hospital Home route (waiting area)
 app.get("/", (req, res) => {
-	res.send(`Hello: This is Port: ${PORT}`);
+	res.send("Hospital waiting area");
 });
 
-app.post()
+// Check your username password and id
+app.get("/health-checkup", (req, res) => {
+	const username = req.headers.username;
+	const password = req.headers.password;
+	const kidneyId = req.query.kidneyId;
+
+	if (!username && !password) return;
+
+	if (username != "nihal" && password == "pass") {
+		res.status(403).json({
+			msg: "User does not exist",
+		});
+		return;
+	}
+
+	if ((username == "nihal") & (password == "pass")) {
+		if (!kidneyId) return;
+
+		if (kidneyId == 1 || kidneyId == 2) {
+			res.status(200).json({
+				msg: `User: ${username} found with kidneyId: ${kidneyId}`,
+			});
+		} else {
+			res.json({
+				msg: "bad input",
+			});
+		}
+	}
+
+	if (kidneyId != 1 || kidneyId != 2) {
+		res.status(411).json({
+			msg: "Wrong Input",
+		});
+		return;
+	}
+
+	res.send(`${username ? username : "User"} is in good health`);
+});
+
+app.post("/", (req, res) => {});
 
 app.listen(PORT, function () {
-	console.log(`server is listening on: http://localhost:${PORT}`);
+	console.log(`Hospital server is listening on: http://localhost:${PORT}`);
 });
