d844d4f27fd18e902a1b1d422432e59889a380f5
nihal <nihalsheikh585@gmail.com>
2026-01-20 22:11:53 +0530

    next() and Middleware

--- 05_middleware-auth-zod/index.js
+++ 05_middleware-auth-zod/index.js
@@ -3,51 +3,67 @@ const app = express();
 
 const PORT = 3000;
 
+function useMiddleware(req, res, next) {
+	console.log("Entered: useMiddleware func");
+	const username = req.headers.username;
+	const password = req.headers.password;
+
+	if (username && password) {
+		console.log("username and password exists");
+	} else {
+		console.log("username and password not found");
+	}
+
+	if (username != "nihal" && password != "pass") {
+		console.log("headers exist, but not matched to this current user");
+		res.status(403).json({
+			msg: "Incorrect Credentials",
+		});
+	} else {
+		console.group("everything was success, routing to next middleware");
+		next();
+	}
+}
+
+function kidneyMiddleware(req, res, next) {
+	console.log("Entered: kidney middleware");
+	const kidneyId = req.query.kidneyId;
+
+	if (kidneyId) {
+		console.log("kidneyId exists");
+	} else {
+		console.log("kidneyId Not Found");
+	}
+
+	if (kidneyId != 1 && kidneyId != 2) {
+		console.log("kidneyId exists, but not the correct one");
+		console.log(kidneyId);
+		res.status(403).json({
+			msg: "Invalid KidneyId",
+		});
+	} else {
+		console.group("everything was success, routing to next middleware");
+		next();
+	}
+}
+
 // Hospital Home route (waiting area)
 app.get("/", (req, res) => {
 	res.send("Hospital waiting area");
 });
 
 // Check your username password and id
-app.get("/health-checkup", (req, res) => {
+app.get("/health-checkup", useMiddleware, (req, res) => {
 	const username = req.headers.username;
-	const password = req.headers.password;
-	const kidneyId = req.query.kidneyId;
-
-	if (!username && !password) return;
-
-	if (username != "nihal" && password == "pass") {
-		res.status(403).json({
-			msg: "User does not exist",
-		});
-		return;
-	}
-
-	if ((username == "nihal") & (password == "pass")) {
-		if (!kidneyId) return;
-
-		if (kidneyId == 1 || kidneyId == 2) {
-			res.status(200).json({
-				msg: `User: ${username} found with kidneyId: ${kidneyId}`,
-			});
-		} else {
-			res.json({
-				msg: "bad input",
-			});
-		}
-	}
-
-	if (kidneyId != 1 || kidneyId != 2) {
-		res.status(411).json({
-			msg: "Wrong Input",
-		});
-		return;
-	}
 
 	res.send(`${username ? username : "User"} is in good health`);
 });
 
-app.post("/", (req, res) => {});
+app.get("/kidney-checkup", useMiddleware, kidneyMiddleware, (req, res) => {
+	const username = req.headers.username;
+
+	res.send(`${username ? username : "User"} has good kidneys`);
+});
 
 app.listen(PORT, function () {
 	console.log(`Hospital server is listening on: http://localhost:${PORT}`);
