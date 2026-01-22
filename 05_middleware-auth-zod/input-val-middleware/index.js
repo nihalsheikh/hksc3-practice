183d1391bd37777ae41c63b152eb732b9d083e13
nihal <nihalsheikh585@gmail.com>
2026-01-22 10:34:28 +0530

    global catches

--- 05_middleware-auth-zod/input-val-middleware/index.js
+++ 05_middleware-auth-zod/input-val-middleware/index.js
@@ -3,8 +3,32 @@ const app = express();
 
 const PORT = 3000;
 
+// before all the routes
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	res.send("Hospital Waiting Area");
+});
+
+app.post("/health-checkup", (req, res) => {
+	// Expecting: kidneys = [1, 2]
+	const kidneys = req.body.kidneys;
+	if (!kidneys) {
+		res.status(404).json({
+			msg: "kidneys not found",
+		});
+
+		return;
+	}
+
+	const kidneyLength = kidneys.length;
+
+	res.send(`Kidney length: ${kidneyLength}`);
+});
+
+// Global Catches
+app.use(function (err, req, res, next) {
+	res.send({ msg: "Error, something is up with the server" });
 });
 
 app.listen(PORT, () => {
