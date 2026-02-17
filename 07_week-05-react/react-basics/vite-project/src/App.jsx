88a9af1a3470b232f8e240f2560b5322ea60cd99
nihal <nihalsheikh585@gmail.com>
2026-02-17 23:32:34 +0530

    state, component and re-render

--- 07_week-05-react/react-basics/vite-project/src/App.jsx
+++ 07_week-05-react/react-basics/vite-project/src/App.jsx
@@ -11,12 +11,13 @@ function App() {
 }
 
 // Button Component
-function CustomButton(props) {
-	function onClickHandler() {
+// props is object
+const CustomButton = (props) => {
+	const onClickHandler = () => {
 		props.setCount(props.count + 1);
-	}
+	};
 
 	return <button onClick={onClickHandler}>Counter {props.count}</button>;
-}
+};
 
 export default App;
