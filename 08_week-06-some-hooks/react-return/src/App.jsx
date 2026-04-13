e5ee99875c42bf2b78a638769895a0d616c12fee
nihal <nihalsheikh585@gmail.com>
2026-04-14 00:12:19 +0530

    maps and key

--- 08_week-06-some-hooks/react-return/src/App.jsx
+++ 08_week-06-some-hooks/react-return/src/App.jsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import Header2 from "./components/Header2";
-import HeaderMemo from "./components/HeaderMemo";
+import Todo from "./components/Todo";
+
+let COUNTER = 4;
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "morning exercise",
+      description: "Workout and weighlifting exercise from 7 am to 8 am",
+    },
+    {
+      id: 2,
+      title: "Shopping",
+      description: "Go to local market, but some vegetables",
+    },
+    {
+      id: 3,
+      title: "Breakfast",
+      description: "after all workout and shopping, have some breakfast",
+    },
+  ]);
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: COUNTER++,
+        title: `new todo ${Math.floor(Math.random() * 10)}`,
+        description: `adding a new todos ${Math.floor(Math.random() * 10)}`,
+      },
+    ]);
+  };
+
   return (
     <>
-      <Header title="This is Luffy" />
+      <Header title="This is Todo App" />
       <br />
+      <button onClick={addTodo}>Add a todo</button>
       <br />
-      <Header2 />
-      <br />
-      <br />
-      <Header title="This is Zoro" />
-      <br />
-      <br />
-      <HeaderMemo title="This is Strawhat Crew" />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <Todo title={todo.title} description={todo.description} />
+        </div>
+      ))}
     </>
   );
 }
---
+++ 08_week-06-some-hooks/react-return/src/components/Todo.jsx
@@ -0,0 +1,10 @@
+const Todo = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Todo;
