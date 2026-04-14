aea2be622d3c9846e599ee820adfa0ae348d85b9
nihal <nihalsheikh585@gmail.com>
2026-04-14 22:54:12 +0530

    useEffect hook

--- 08_week-06-some-hooks/react-return/src/App.jsx
+++ 08_week-06-some-hooks/react-return/src/App.jsx
@@ -1,16 +1,22 @@
-import CardWrapper from "./components/CardWrapper";
-import TextComponent from "./components/TextComponent";
-import TextComponent2 from "./components/TextComponent2";
+import { useEffect } from "react";
+// import CardWrapper from "./components/CardWrapper";
+// import TextComponent from "./components/TextComponent";
+// import TextComponent2 from "./components/TextComponent2";
 
 function App() {
+  useEffect(() => {
+    alert("Hi");
+  }, []);
+
   return (
     <>
-      <CardWrapper>
+      {/* <CardWrapper>
         <TextComponent />
       </CardWrapper>
       <CardWrapper>
         <TextComponent2 />
-      </CardWrapper>
+      </CardWrapper> */}
+      <div>home</div>
     </>
   );
 }
