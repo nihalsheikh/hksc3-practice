137188481e0f0f5b54d70942f92970e534565ede
nihal <nihalsheikh585@gmail.com>
2026-04-16 23:31:12 +0530

    comments

--- 08_week-06-some-hooks/react-return/src/components/CardWrapper.jsx
+++ 08_week-06-some-hooks/react-return/src/components/CardWrapper.jsx
@@ -1,6 +1,7 @@
 const CardWrapper = ({ children }) => {
   return (
     <div className="wrapper">
+      {/* passing/rendering children as props */}
       {children}
       this is a Wrapper component
     </div>
